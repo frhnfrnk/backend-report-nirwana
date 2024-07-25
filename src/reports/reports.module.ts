6a0a711ed11a677d5336fb85fd11d65182476a92
Farhan Franaka <farhanfranaka0505@gmail.com>
2024-07-25 12:38:54 +0800

    fix build

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -3,11 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Report, ReportSchema } from './report.schema';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from 'src/auth/roles.guard';
+import { RolesGuard } from '../auth/roles.guard';
 import { PassportModule } from '@nestjs/passport';
-import { AuthGuard } from 'src/auth/auth.guard';
 
 @Module({
   imports: [
